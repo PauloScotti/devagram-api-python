71efcd042bfd00e5f09cfd2641a9b97e1b310230
PauloScotti <paulo.scotti@hotmail.com>
2022-05-20 09:42:10 -0300

    Autenticação e Integração

--- routes/AutenticacaoRoute.py
+++ routes/AutenticacaoRoute.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Body, HTTPException
 
 from models.UsuarioModel import UsuarioLoginModel
-from services.AuthService import login_service
+from services.AuthService import login_service, gerar_token_jwt
 
 router = APIRouter()
 
@@ -10,9 +10,13 @@ router = APIRouter()
 async def login(usuario: UsuarioLoginModel = Body(...)):
     resultado = await login_service(usuario)
 
-    if not resultado['status'] == 201:
+    if not resultado['status'] == 200:
         raise HTTPException(status_code=resultado['status'], detail=resultado['mensagem'])
 
     del resultado['dados']['senha']
+
+    token = gerar_token_jwt(resultado['dados']['id'])
+
+    resultado['token'] = token
 
     return resultado
